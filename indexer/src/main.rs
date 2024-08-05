17dc0319a0850ea263a56078de547364523b45a6
Spring Chiu <fospring@outlook.com>
2024-08-05 16:29:29 +0800

    feat: indexer add get blocks

--- indexer/src/main.rs
+++ indexer/src/main.rs
@@ -1,3 +1,6 @@
+use std::{thread, time::Duration};
+
+use solana_client::rpc_client::RpcClient;
 use solana_client::{
     pubsub_client::PubsubClient,
     rpc_config::{RpcTransactionLogsConfig, RpcTransactionLogsFilter},
@@ -20,13 +23,39 @@ fn main() {
     .unwrap();
 
     let mut counter = 0;
+    let mut slots: Vec<u64> = vec![];
     loop {
         let data = reciver.recv().unwrap();
-        println!("is_err: {}, signature: {}, logs: {:?}", data.value.err.is_some(),data.value.signature.clone(), data.value.logs);
+        slots.push(data.context.slot);
+        println!(
+            "slot: {}, is_err: {}, signature: {}, logs: {:?}",
+            data.context.slot,
+            data.value.err.is_some(),
+            data.value.signature.clone(),
+            data.value.logs
+        );
 
         counter += 1;
-        if counter > 2 {
+        if counter > 1 {
             break;
+        }
+    }
+
+    thread::sleep(Duration::from_secs(20));
+    let url = "http://127.0.0.1:8899".to_string();
+    let client = RpcClient::new(url);
+    for slot in slots {
+        let block = client.get_block(slot).unwrap();
+        println!("block: {:?}", block);
+        for tx in block.transactions {
+            println!(
+                "tx in block: {} transactions: {:?}",
+                block.parent_slot + 1,
+                tx
+            );
+            if let Some(meta) = tx.meta {
+                println!("meta.log_messages: {:?}", meta.log_messages);
+            }
         }
     }
 
